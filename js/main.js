23ea16911dd19b2fa74cc5f7f53f58767814f64c
Bill <bunnkywhat@gmail.com>
2022-05-12 15:23:29 -0400

    Added: Tier changing and button shake

--- js/main.js
+++ js/main.js
@@ -6,7 +6,44 @@ var rgp = 0;
 function rgpClick(number){
     rgp = rgp + number;
     document.getElementById("rgp").innerHTML = rgp;
+    changeTier();
 };
+
+//////////////////////////
+//Change tier
+//////////////////////////
+
+function changeTier(){
+    tier = 0;
+    if (rgp != 0 && rgp <= 100){
+        tier = 0;
+        document.getElementById("tierimg").src = "img/tier_0.png";
+
+    } else if (rgp >= 100 && rgp <= 200){
+        tier = 1;
+        document.getElementById("tierimg").src = "img/tier_1.png";
+
+    } else if(rgp >= 200 && rgp <= 300){
+        tier = 2; 
+        document.getElementById("tierimg").src = "img/tier_2.png";
+
+    } else if(rgp >= 300 && rgp <= 400){
+        tier = 3; 
+        document.getElementById("tierimg").src = "img/tier_3.png";
+
+    } else if(rgp >= 400 && rgp <= 500){
+        tier = 4;
+        document.getElementById("tierimg").src = "img/tier_4.png";
+
+    } else if(rgp >= 500 && rgp <= 600){
+        tier = 5;
+        document.getElementById("tierimg").src = "img/tier_5.png";
+
+    } else if(rgp >= 600 && rgp <= 700){
+        tier = 6;
+        document.getElementById("tierimg").src = "img/tier_6.png";
+    }
+}
 
 //////////////////////////
 //Buy Orbs Function
@@ -14,17 +51,21 @@ function rgpClick(number){
 var orbs = 0;
 
 function buyRgpOrb(){
-var orbCost = Math.floor(10 * Math.pow(1.1,orbs));
-if (orbs <= orbCost && rgp >= orbCost){
-    orbs = orbs + 1;
-    rgp = rgp - orbCost;
-    document.getElementById('orbs').innerHTML = orbs;
-    document.getElementById('rgp').innerHTML = rgp;    
-} else {
-    alert("You do not have enough RGP!");
-};
-var nextCost = Math.floor(10 * Math.pow(1.1,orbs));
-document.getElementById('orbCost').innerHTML = nextCost;
+    var orbCost = Math.floor(10 * Math.pow(1.1,orbs));
+    var element = document.getElementById("orbbuy");
+
+    if (orbs <= orbCost && rgp >= orbCost){    
+        orbs = orbs + 1;        //Add 1 Orb
+        rgp = rgp - orbCost;    //Subtract Orb Cost
+
+        document.getElementById('orbs').innerHTML = orbs;   //Set Orb amount text
+        document.getElementById('rgp').innerHTML = rgp;     //Set RGP amount text
+    } else {
+        element.classList.toggle("no");
+    }
+
+    var nextCost = Math.floor(10 * Math.pow(1.1,orbs));
+    document.getElementById('orbCost').innerHTML = nextCost;
 }
 
 
